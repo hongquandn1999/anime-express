f286031a1804d50e516af67563098cf4b5ec8cec
Quân Huỳnh <huynhquandn1999@gmail.com>
2020-04-06 09:30:37 +0700

    Add file pug and complete express no db

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ var port = 3000;
 // temple engine
 app.set("view engine", "pug");
 app.set("views", "./views");
+// body
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+var characters = [
+    { id: 1, name: "Mimosa" },
+    { id: 2, name: "Noelle" },
+];
 
 app.get("/", function(req, res) {
     res.render("index");
@@ -12,11 +20,29 @@ app.get("/", function(req, res) {
 
 app.get("/character", function(req, res) {
     res.render("character/index", {
-        users: [
-            { id: 1, name: "Mimosa" },
-            { id: 2, name: "Noelle" },
-        ],
+        characters: characters,
     });
+});
+
+app.get("/character/search", function(req, res) {
+    var q = req.query.q;
+
+    var matchedCharacter = characters.filter(function(character) {
+        return character.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render("character/index", {
+        characters: matchedCharacter,
+        valued: q,
+    });
+});
+
+app.get("/character/create", function(req, res) {
+    res.render("character/create");
+});
+
+app.post("/character/create", function(req, res) {
+    characters.push(req.body);
+    res.redirect("/character");
 });
 
 app.listen(port, function() {
